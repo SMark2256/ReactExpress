503fcb23b523f9981610263ae8d9815be4dad1f7
Siklodi Mark <smark.siklodi@gmail.com>
2022-07-05 11:18:27 +0200

    code cleaning

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react'
-//import { Route, BrowserRouter as Router } from 'react-router-dom'
 //components
 import FoodsList from './components/Foodslist'
 import Footer from './components/Footer'
 import Header from './components/Header'
-//import Home from './pages/Home.js'
 import Cart from './components/Cart'
 //util
 import fetchMoviesHandler from './util/Getdata.js'
@@ -13,7 +11,7 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
-function App() {
+const App = () => {
   const [foods, setFoods] = useState([])
   const [foodtype, setFoodtype] = useState('best-foods')
   const [isLoading, setLoading] = useState(false)
@@ -25,19 +23,15 @@ function App() {
   }, [foodtype]);
 
   return (
-    <React.Fragment>
+    <>
       <Header inCart={inCart} foods={foods} foodtype={foodtype} setShow={setShow} show={show} fetchMoviesHandler={fetchMoviesHandler}/>
       <Cart foods={foods}/>
-     <section className='foodtable'>
-        {show && foods.length > 0 && 
-          <FoodsList inCart={inCart} setinCart={setinCart} isLoading={isLoading} setFoodtype={setFoodtype} foods={foods}/>
-        }
-        {show && !isLoading && foods.length === 0 && 
-          <p className='text-center text-bold'>Found no foods</p>
-        }
+      <section className='foodtable'>
+        {show && foods.length > 0 && <FoodsList inCart={inCart} setinCart={setinCart} isLoading={isLoading} setFoodtype={setFoodtype} foods={foods}/>}
+        {show && !isLoading && foods.length === 0 && <p className='text-center text-bold'>Found no foods</p>}
       </section>
       <Footer/>
-    </React.Fragment>
+    </>
   );
 }
 
